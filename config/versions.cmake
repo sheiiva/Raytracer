348b622526e65f2711b3b618e2e4b65a7b79d569
Corentin COUTRET ROZET <corentin.rozet@epitech.eu>
2023-03-21 00:22:08 +0100

    [UP] - Move googletests fetch package to config management

---
+++ config/versions.cmake
@@ -0,0 +1,8 @@
+### versions.cmake: CMake file for version definitions of the project
+###
+### 2023, March 21
+### Author: Corentin COUTRET-ROZET (corentin.rozet@epitech.eu)
+###
+### Copyright 2023, Corentin COUTRET-ROZET
+
+set(GTEST_VERSION   release-1.12.1)
